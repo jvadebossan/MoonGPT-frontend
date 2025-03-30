bea6f8b0912877e68738548be7cfd05092c6db39
jvadebossan <jvdebossan@gmail.com>
2025-03-30 18:10:30 -0300

    Add auth box template and fix theme

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,11 +1,45 @@
+import { Box, Grid2, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
+import Signup from "./Signup";
+import Login from "./Login";
+import { useTranslation } from "react-i18next";
 
 export default function AuthPage() {
   const { type } = useParams();
+  const { t } = useTranslation();
+
+  console.log(type);
   return (
-    <div>
-      {type === "login" && <h1>Login Page</h1>}
-      {type === "signup" && <h1>Signup Page</h1>}
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ width: "100%", height: "100%" }}
+    >
+      <Grid2
+        container
+        justifyContent="flex-start"
+        alignItems="center"
+        direction="column"
+        sx={{
+          minWidth: "350px",
+          minHeight: "400px",
+          width: "24%",
+          height: "55%",
+          border: "2px solid",
+          borderColor: "border.main",
+          borderRadius: "10px",
+          mb: "10%",
+          p: 3,
+        }}
+      >
+        <Grid2 size={12} sx={{ textAlign: "center", py: 2 }}>
+          <Typography variant="h6" fontWeight={600} color="text.secondary">
+            {type === "signup" ? t("auth.signupMsg") : t("auth.loginMsg") }
+          </Typography>
+        </Grid2>
+        {type === "signup" ? <Signup /> : <Login />}
+      </Grid2>
+    </Box>
   );
 }
--- src/providers/theme.ts
+++ src/providers/theme.ts
@@ -1,31 +1,47 @@
-import { createTheme, PaletteMode } from '@mui/material';
+import { createTheme, PaletteMode } from "@mui/material";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    border: {
+      main: string;
+    };
+  }
+  interface PaletteOptions {
+    border?: {
+      main?: string;
+    };
+  }
+}
 
 export const createCustomTheme = (mode: PaletteMode) => {
   return createTheme({
     palette: {
       mode,
       primary: {
-        main: mode === 'dark' ? '#F8FAFC' : '#0F172A',
+        main: mode === "dark" ? "#F8FAFC" : "#0F172A",
       },
       secondary: {
-        main: mode === 'dark' ? '#E2E8F0' : '#334155',
+        main: mode === "dark" ? "#E2E8F0" : "#334155",
       },
       background: {
-        default: mode === 'dark' ? '#0F172A' : '#F8FAFC',
-        paper: mode === 'dark' ? '#1E293B' : '#FFFFFF',
+        default: mode === "dark" ? "#0F172A" : "#F8FAFC",
+        paper: mode === "dark" ? "#1E293B" : "#FFFFFF",
       },
       text: {
-        primary: mode === 'dark' ? '#E2E8F0' : '#0F172A',
-        secondary: mode === 'dark' ? '#CBD5E1' : '#475569',
+        primary: mode === "dark" ? "#E2E8F0" : "#0F172A",
+        secondary: mode === "dark" ? "#64748B" : "#94A3B8",
       },
       error: {
-        main: mode === 'dark' ? '#F87171' : '#EF4444',
+        main: mode === "dark" ? "#F87171" : "#EF4444",
       },
       success: {
-        main: mode === 'dark' ? '#4ADE80' : '#10B981',
+        main: mode === "dark" ? "#4ADE80" : "#10B981",
       },
       warning: {
-        main: mode === 'dark' ? '#FBBF24' : '#F59E0B',
+        main: mode === "dark" ? "#FBBF24" : "#F59E0B",
+      },
+      border: {
+        main: mode === "light" ? "#E2E8F0" : "#334155",
       },
     },
     components: {
@@ -33,18 +49,17 @@ export const createCustomTheme = (mode: PaletteMode) => {
         styleOverrides: {
           root: {
             // Default button style for all variants
-            '&.MuiButton-contained': {
-              backgroundColor: mode === 'light' ? '#0F172A' : '#E2E8F0',
-              color: mode === 'light' ? '#E2E8F0' : '#0F172A',
-              '&:hover': {
-                backgroundColor: mode === 'light' ? '#334155' : '#F8FAFC',
+            "&.MuiButton-contained": {
+              backgroundColor: mode === "light" ? "#0F172A" : "#E2E8F0",
+              color: mode === "light" ? "#E2E8F0" : "#0F172A",
+              "&:hover": {
+                backgroundColor: mode === "light" ? "#334155" : "#F8FAFC",
               },
             },
-            textTransform: 'capitalize',
+            textTransform: "capitalize",
           },
         },
       },
     },
-  })
-
+  });
 };
